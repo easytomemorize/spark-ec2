c6394295c3d9cef33b3cb0fdea2f94d4a756c33f
Babar Tareen <babar.tareen@rootmetrics.com>
2016-02-25 13:40:20 -0800

    Updated scala to version 2.11.7

--- scala/init.sh
+++ scala/init.sh
@@ -7,14 +7,18 @@ if [ -d "scala" ]; then
   return 0
 fi
 
-SCALA_VERSION="2.10.3"
+SCALA_VERSION="2.11.7"
 
 if [[ "0.7.3 0.8.0 0.8.1" =~ $SPARK_VERSION ]]; then
   SCALA_VERSION="2.9.3"
 fi
 
 echo "Unpacking Scala"
-wget http://s3.amazonaws.com/spark-related-packages/scala-$SCALA_VERSION.tgz
+if [ "2.11.7" == $SCALA_VERSION  ]; then
+  wget http://downloads.lightbend.com/scala/2.11.7/scala-2.11.7.tgz
+else  
+  wget http://s3.amazonaws.com/spark-related-packages/scala-$SCALA_VERSION.tgz
+fi  
 tar xvzf scala-*.tgz > /tmp/spark-ec2_scala.log
 rm scala-*.tgz
 mv `ls -d scala-* | grep -v ec2` scala
